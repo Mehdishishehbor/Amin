b40800c43027c44568df90085e04659e47d58c4c
Mehdi Shishehbor <mehdi.shishehbor@gmail.com>
2022-03-07 16:46:20 -0800

    Save mat files for matlab.

--- notebooks/Demo_borehole1D_lmgp2.py
+++ notebooks/Demo_borehole1D_lmgp2.py
@@ -145,18 +145,8 @@ from scipy.io import savemat
 savemat('borehole_100.mat',{'Xtrain':train_x.numpy(), 'Xtest':test_x.numpy(), 'ytrain':train_y.numpy(), 'ytest':test_y.numpy()})
 
 
-# ## Creating a LVGP instance
-# 
-# We begin by defining the `LVGPR` instance. The required input arguments are the training data (`train_x` and `train_y`), the indices for the qualitative (`qual_index`) and quantitative (`quant_index`) variables, and the number of levels for each qualitative variable (`num_levels_per_var`), whose entries are specified in the same as order as that in the index list. The `InputSpace` object automatically generates the latter three entries.
-# 
-# There are other arguments, which have default values. Among them, the important one is the type of the correlation kernel for the quantitative inputs (`quant_correlation_class`). Available options are `'RBFKernel'`(default), `Matern52Kernel` (twice-differentiable) and `Matern32Kernel`(once-differentiable). 
-
-# In[5]:
-
-
-# create LVGP instance
 set_seed(4)
-model = LMGP(
+model2 = LMGP(
     train_x=train_x,
     train_y=train_y,
     qual_index=config.qual_index,
@@ -165,57 +155,36 @@ model = LMGP(
     quant_correlation_class="RBFKernel",
 ).double()
 
-# print model structure
-model
 
-# Define prior
-
-
-# Note that the hyperparameters of the model are not yet optimized!
-# 
-# ## Optimization using multiple random starts
-# 
-# There are two optimization methods available in the package. In the first method, all hyperparameters are jointly optimized using multi-start numerical optimization with **L-BFGS** as the optimization algorithm.
-
-# In[6]:
-
-
-# fit model with 10 different starts
-reslist,nll_inc = fit_model_scipy(
-    model,
-    num_restarts=0, # number of starting points
+# optimize noise successively
+nll_inc_tuned,opt_history = noise_tune(
+    model2, 
+    num_restarts = 0 # number of restarts in the initial iteration
 )
 
-# set model to eval model; default is in train model
-_ = model.eval()
-
-
-# In[7]:
-
+# 
+print('NLL obtained from noise tuning strategy.......: %6.2f'%nll_inc_tuned)
 
 # prediction on test set
 with torch.no_grad():
-    # set return_std = False if standard deviation is not needed 
-    test_mean,test_std = model.predict(test_x,return_std=True)
-
+    # set return_std = False if standard deviation is not needed
+    test_mean2 = model2.predict(test_x,return_std=False)
+    
 
 
 print('######################################')
-noise = model.likelihood.noise_covar.noise.item() * train_y.std()**2
+noise = model2.likelihood.noise_covar.noise.item() * train_y.std()**2
 print(f'The estimated noise parameter is {noise}')
 
 # print MSE
-mse = ( (test_y-test_mean)**2).mean()
+mse = ( (test_y-test_mean2)**2).mean()
 print('MSE : %5.3f'%mse.item())
 
+
 # print RRMSE
-rrmse = torch.sqrt(((test_y-test_mean)**2).mean()/((test_y-test_y.mean())**2).mean())
-print('RRMSE : %5.3f'%rrmse.item())
+rrmse = torch.sqrt(((test_y-test_mean2)**2).mean()/((test_y-test_y.mean())**2).mean())
+print('Test RRMSE with noise-tuning strategy : %5.3f'%rrmse.item())
 
-
-# The test RRMSE seems to be pretty good. We now plot the estimated latent variables for the levels of $r_w$ and $H_l$. 
-
-# In[ ]:
 
 
 
@@ -224,7 +193,9 @@ end_time = time.time()
 print(f'The total time in second is {end_time - start_time}')
 
 # plot latent values
-plot_latent.plot_ls(model, constraints_flag= True)
+plot_latent.plot_ls(model2, constraints_flag= True)
 
-plt.plot(test_y, test_mean, 'ro')
+plt.plot(test_y, test_mean2, 'ro')
 plt.show()
+
+
