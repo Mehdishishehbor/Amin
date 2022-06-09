4efb440ab6fc612b078bb5b7fdcd2c2338e36f47
Mehdi Shishehbor <mehdi.shishehbor@gmail.com>
2022-06-08 18:29:21 -0700

    fixed the standard and setlevels

--- lmgp_pytorch/preprocessing/Normalize.py
+++ lmgp_pytorch/preprocessing/Normalize.py
@@ -4,7 +4,8 @@ import torch
 
 
 def standard(Xtrain, quant_index, Xtest = None):
-
+    if len(quant_index) == 0:
+        return Xtrain
     mean_xtrain =  Xtrain[...,quant_index].mean(axis = 0)
     std_xtrain =  Xtrain[...,quant_index].std(axis = 0)
     Xtrain[...,quant_index] = (Xtrain[..., quant_index] - mean_xtrain )/std_xtrain
--- lmgp_pytorch/preprocessing/numericlevels.py
+++ lmgp_pytorch/preprocessing/numericlevels.py
@@ -1,11 +1,22 @@
 import torch
+import numpy as np
 
 def setlevels(X, qual_index):
+    if type(X) == torch.Tensor:
+        temp = X.copy()
     if X.ndim > 1:
         for j in qual_index:
             a = X[..., j]
-            X[..., j] = torch.tensor([*map(lambda m: list(set(sorted(a.tolist()))).index(m), a)]).to(X)
+            X[..., j] = torch.tensor([*map(lambda m: list(sorted(set(a.tolist()))).index(m), a)])
     else:
-        X = torch.tensor([*map(lambda m: list(set(sorted(X.tolist()))).index(m), X)]).to(X)
-    return X
+        X = torch.tensor([*map(lambda m: list(set(sorted(X.tolist()))).index(m), X)])
+    
+
+    X = X.astype(float)
+
+    if type(X) == np.ndarray:
+        X = torch.from_numpy(X)
+        return X
+    else:
+        X.to(temp)
 
